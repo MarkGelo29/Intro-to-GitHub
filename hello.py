2c8021814f48628f26640e8c9fc92c45f7fd94fb
MarkGelo29 <markangelo.panchorivera2003@gmail.com>
2025-10-18 01:51:44 +0800

    Revised hello.py and notes.txt

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # hello.py
 def main():
-    print("Hello from Intro-to-GitHub repo!")
+    print("Assignment 3: Introduction to GitHub - Hello from Intro-to-GitHub repo!")
 
 if __name__ == "__main__":
     main()
